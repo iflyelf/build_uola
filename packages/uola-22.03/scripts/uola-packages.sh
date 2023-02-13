66bfea16345455f588d2de032ccbe088d5cfd11c
danxiaonuo <iflyelf@gmail.com>
2023-02-13 16:32:34 +0800

    init

--- packages/uola-22.03/scripts/uola-packages.sh
+++ packages/uola-22.03/scripts/uola-packages.sh
@@ -10,20 +10,20 @@ sed -i '$a cgi-timeout = 300' net/uwsgi/files-luci-support/luci-webui.ini
 #find "${GITHUB_WORKSPACE}/packages/${matrix_target}/patches" -type f -name '*.patch' -print0 | sort -z | xargs -I % -t -0 -n 1 sh -c "cat '%'  | patch -d './' -p1 -E --forward --no-backup-if-mismatch"
 
 # 增加 smartdns
-rm -rf smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns.git smartdns
-find ${GITHUB_WORKSPACE}/uola-packages/ -type d -regextype posix-extended -regex '.*.(git|svn)' | xargs -exec rm -rf
+rm -rf net/smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns.git net/smartdns
+find ${GITHUB_WORKSPACE}/uola-packages/net/ -type d -regextype posix-extended -regex '.*.(git|svn)' | xargs -exec rm -rf
 mkdir -p smartdns/conf
-cat ${GITHUB_WORKSPACE}/packages/${matrix_target}/server/smartdns/conf/smartdns.conf > smartdns/conf/smartdns.conf
-cat ${GITHUB_WORKSPACE}/packages/${matrix_target}/server/smartdns/conf/custom.conf > smartdns/conf/custom.conf
-wget https://raw.githubusercontent.com/danxiaonuo/gwf/main/smartdns/smartdns_xiaonuo_domain.conf -O smartdns/conf/smartdns_xiaonuo_domain.conf
-wget https://raw.githubusercontent.com/danxiaonuo/gwf/main/smartdns/smartdns_gfw_domain.conf -O smartdns/conf/smartdns_gfw_domain.conf
-sed -i -e "s@\$(PKG_BUILD_DIR)/package/openwrt/custom.conf@\$(CURDIR)/conf/custom.conf@g" smartdns/Makefile
-sed -i -e "s@\$(PKG_BUILD_DIR)/package/openwrt/files/etc/config/smartdns@\$(CURDIR)/conf/smartdns.conf@g" smartdns/Makefile
-sed -i '/conffiles/a\/etc/smartdns/smartdns_gfw_domain.conf' smartdns/Makefile
-sed -i '/conffiles/a\/etc/smartdns/smartdns_xiaonuo_domain.conf' smartdns/Makefile
-sed -i '/blacklist-ip.conf/a\\t$(INSTALL_CONF) $(CURDIR)/conf/smartdns_gfw_domain.conf $(1)/etc/smartdns/smartdns_gfw_domain.conf' smartdns/Makefile
-sed -i '/smartdns_gfw_domain.conf/a\\t$(INSTALL_CONF) $(CURDIR)/conf/smartdns_xiaonuo_domain.conf $(1)/etc/smartdns/smartdns_xiaonuo_domain.conf' smartdns/Makefile
+cat ${GITHUB_WORKSPACE}/packages/${matrix_target}/server/smartdns/conf/smartdns.conf > net/smartdns/conf/smartdns.conf
+cat ${GITHUB_WORKSPACE}/packages/${matrix_target}/server/smartdns/conf/custom.conf > net/smartdns/conf/custom.conf
+wget https://raw.githubusercontent.com/danxiaonuo/gwf/main/smartdns/smartdns_xiaonuo_domain.conf -O net/smartdns/conf/smartdns_xiaonuo_domain.conf
+wget https://raw.githubusercontent.com/danxiaonuo/gwf/main/smartdns/smartdns_gfw_domain.conf -O net/smartdns/conf/smartdns_gfw_domain.conf
+sed -i -e "s@\$(PKG_BUILD_DIR)/package/openwrt/custom.conf@\$(CURDIR)/conf/custom.conf@g" net/smartdns/Makefile
+sed -i -e "s@\$(PKG_BUILD_DIR)/package/openwrt/files/etc/config/smartdns@\$(CURDIR)/conf/smartdns.conf@g" net/smartdns/Makefile
+sed -i '/conffiles/a\/etc/smartdns/smartdns_gfw_domain.conf' net/smartdns/Makefile
+sed -i '/conffiles/a\/etc/smartdns/smartdns_xiaonuo_domain.conf' net/smartdns/Makefile
+sed -i '/blacklist-ip.conf/a\\t$(INSTALL_CONF) $(CURDIR)/conf/smartdns_gfw_domain.conf $(1)/etc/smartdns/smartdns_gfw_domain.conf' net/smartdns/Makefile
+sed -i '/smartdns_gfw_domain.conf/a\\t$(INSTALL_CONF) $(CURDIR)/conf/smartdns_xiaonuo_domain.conf $(1)/etc/smartdns/smartdns_xiaonuo_domain.conf' net/smartdns/Makefile
 
 # find ${GITHUB_WORKSPACE}/uola-packages/ -type d -regextype posix-extended -regex '.*.(git|github|svn)' | xargs -exec rm -rf
 # find ${GITHUB_WORKSPACE}/uola-packages/ -type f -regextype posix-extended -regex '.*.(LICENSE|README|MAINTAINERS)' | xargs -exec rm -rf
