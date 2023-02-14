6d1c020af57e6f834a7c5d3082e050d282c5f1d3
danxiaonuo <iflyelf@gmail.com>
2023-02-14 16:53:04 +0800

    init

--- custom/uola-22.03/scripts/uola-custom.sh
+++ custom/uola-22.03/scripts/uola-custom.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# 增加 default-settings
+rm -rf default-settings
+\cp -rfp ${GITHUB_WORKSPACE}/custom/${matrix_target}/server/default-settings default-settings
+
 # find ${GITHUB_WORKSPACE}/uola-custom/ -type d -regextype posix-extended -regex '.*.(git|github|svn)' | xargs -exec rm -rf
 # find ${GITHUB_WORKSPACE}/uola-custom/ -type f -regextype posix-extended -regex '.*.(LICENSE|README|MAINTAINERS)' | xargs -exec rm -rf
 
