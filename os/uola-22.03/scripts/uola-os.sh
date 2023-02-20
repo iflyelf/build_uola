cc0aef19fd1405bd739820145a2afc4fa20ae2a6
danxiaonuo <iflyelf@gmail.com>
2023-02-20 10:27:47 +0800

    init

--- os/uola-22.03/scripts/uola-os.sh
+++ os/uola-22.03/scripts/uola-os.sh
@@ -3,6 +3,9 @@
 # 修改源地址
 cat ${GITHUB_WORKSPACE}/os/${matrix_target}/server/feeds.conf.default > feeds.conf.default
 
+# 增加os源
+git clone --depth=1 https://github.com/iflyelf/uola-custom.git package/xiaonuo
+
 # 修改LuCI 配置
 sed -i '/odhcp6c/d' include/target.mk
 sed -i '/odhcpd-ipv6only/d' include/target.mk
@@ -11,13 +14,13 @@ sed -i '/base-files/i\\        default-settings \\' include/target.mk
 
 # 增加zsh
 mkdir -p package/base-files/files/root
-git clone https://github.com/ohmyzsh/ohmyzsh.git package/base-files/files/root/.oh-my-zsh
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git package/base-files/files/root/.oh-my-zsh
 rm -rf package/base-files/files/root/.oh-my-zsh/.git
-git clone https://github.com/zsh-users/zsh-autosuggestions package/base-files/files/root/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions package/base-files/files/root/.oh-my-zsh/plugins/zsh-autosuggestions
 rm -rf package/base-files/files/root/.oh-my-zsh/plugins/zsh-autosuggestions/.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git package/base-files/files/root/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git package/base-files/files/root/.oh-my-zsh/plugins/zsh-syntax-highlighting
 rm -rf package/base-files/files/root/.oh-my-zsh/plugins/zsh-syntax-highlighting/.git
-git clone https://github.com/zsh-users/zsh-completions package/base-files/files/root/.oh-my-zsh/plugins/zsh-completions
+git clone --depth=1 https://github.com/zsh-users/zsh-completions package/base-files/files/root/.oh-my-zsh/plugins/zsh-completions
 rm -rf package/base-files/files/root/.oh-my-zsh/plugins/zsh-completions/.git
 cat ${GITHUB_WORKSPACE}/os/common/server/package/zsh/.zshrc > package/base-files/files/root/.zshrc
 
